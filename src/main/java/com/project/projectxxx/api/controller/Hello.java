f366d3cacb748922f12d16c331f6b8e3cd0542b9
HyunSeok Yu <yuhs7890@gmail.com>
2025-04-08 18:28:38 +0900

    configuration commit

--- src/main/java/com/project/projectxxx/api/controller/Hello.java
+++ src/main/java/com/project/projectxxx/api/controller/Hello.java
@@ -1,12 +1,16 @@
 package com.project.projectxxx.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="helloController",description="helloController test입니다")
 @RestController
 @RequiredArgsConstructor
 public class Hello {
+    @Operation(summary = "helloController입니다" , description = "test입니다")
     @GetMapping("/hello")
     public String hello(){
         return "hello";
